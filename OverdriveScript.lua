0786333f3001bd1aebb30da4bc50ba6ac29936e8
morenoffproScriptsRoblox <morenoff.pro@gmail.com>
2026-09-14 11:54:50 -0300

    Update script loading URL in OverdriveScript.lua

--- OverdriveScript.lua
+++ OverdriveScript.lua
@@ -1,3 +1,3 @@
 getgenv().SCRIPT_KEY = "key"
 
-loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/bc659442d01479c98f4db5487a78d0b1bb84a42f359347fbe075ee6124f3d963/download"))()
+loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/5f17f1010cd153c05b692082ce482e8cc151035f06797b2e8f0b55df8c9002cb/download"))()
